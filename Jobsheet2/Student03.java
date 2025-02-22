2ee82a55f47830dd02d4ab450d9212966605f274
Angelinat3 <fangelinatj@gmail.com>
2025-02-22 22:44:11 +0700

    jobsheet2

---
+++ Jobsheet2/Student03.java
@@ -0,0 +1,75 @@
+<<<<<<< HEAD
+package Jobsheet2;
+
+public class Student03 {
+    static String studentID;
+    String name;
+    String className;
+    double gpa;
+
+    void print() {
+        System.out.println("Student ID : " + studentID);
+        System.out.println("Name : " + name);
+        System.out.println("Class : " + className);
+        System.out.println("GPA : " + gpa);
+    }
+
+    void changeClass(String newClass) {
+        className = newClass;
+    }
+
+    void updateGPA(double newGPA) {
+        gpa = newGPA;
+    }
+
+    String evaluate() { 
+        if (gpa >= 3.5) {
+            return "Excellent";
+        } else if (gpa >= 3.0) {
+            return "Good";
+        } else if (gpa >= 2.0) {
+            return "Fair";
+        } else if (gpa >= 0.0) {
+            return "Poor";
+        } else{
+            return "Invalid IPK. Must be between 0.0 and 4.0.";
+        }
+    }
+}
+=======
+package Jobsheet2;
+
+public class Student03 {
+    static String studentID;
+    String name;
+    String className;
+    double gpa;
+
+    void print() {
+        System.out.println("Student ID : " + studentID);
+        System.out.println("Name : " + name);
+        System.out.println("Class : " + className);
+        System.out.println("GPA : " + gpa);
+    }
+
+    void changeClass(String newClass) {
+        className = newClass;
+    }
+
+    void updateGPA(double newGPA) {
+        gpa = newGPA;
+    }
+
+    String evaluate() { 
+        if (gpa >= 3.5) {
+            return "Excellent";
+        } else if (gpa >= 3.0) {
+            return "Good";
+        } else if (gpa >= 2.0) {
+            return "Fair";
+        } else {
+            return "Poor";
+        }
+    }
+}
+>>>>>>> origin/main
